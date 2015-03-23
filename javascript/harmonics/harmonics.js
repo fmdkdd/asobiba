1fe3b1ab8ad5200511adc7958afa2cd4b3813a3e
fmdkdd <fmdkdd@gmail.com>
2015-03-23 23:28:52 +0100

    laa-la-la la-laaa

--- javascript/harmonics/harmonics.js
+++ javascript/harmonics/harmonics.js
@@ -1,6 +1,6 @@
 var ctx = new AudioContext()
 var volume = ctx.createGain()
-volume.gain.value = 0.3
+volume.gain.value = 0.1
 volume.connect(ctx.destination)
 
 function O(freq = 440, amp = 1, type = 'sine') {
@@ -15,39 +15,100 @@ function O(freq = 440, amp = 1, type = 'sine') {
 
   return o}
 
-function h(n) {
-  return O(440 * n, 1 / Math.pow(2, n))}
+function H(/* fundamentals */) {
+  var fundamentals = Array.from(arguments)
+  var waves = fundamentals.map(f => O(440 * f, 1 / f))
+  return {
+    start(t = ctx.currentTime) {waves.forEach(w => w.start(t))}
+    ,stop(t = ctx.currentTime) {waves.forEach(w => w.stop(t))}}}
 
-// Sine wave
-//h(1).start()
+function C(/* freqs */) {
+  var fundamentals = Array.from(arguments)
+  var waves = fundamentals.map(f => O(440 * f))
+  return {
+    start() {waves.forEach(w => w.start())}
+    ,stop() {waves.forEach(w => w.stop())}}}
 
-// Saw wave
-//;[1,2,3,4,5,6].map(h).forEach(o => o.start())
+var channel = (f) => ({
+  start() { this.sound = f()
+            this.sound.start()
+            this.playing = true }
+  ,stop() { this.sound.stop()
+            this.playing = false }
+  ,toggle() { if (this.playing) this.stop()
+              else this.start() }})
 
-// Pulse wave
-//;[1,3,5,7,9].map(h).forEach(o => o.start())
+var acSquare = channel(_ => O(440, 1, 'square'))
+var sine = channel(_ => O(440, 1, 'sine'))
+var sinesSquare = channel(_ => H(1,3,5,7,9,11,13))
+var sinesSaw = channel(_ => H(1,2,3,5,6,7,8,9,10))
+var sinesEven = channel(_ => H(1,2,4,6,8,10))
+var majorChordHarm = channel(_ => H(1,5/4,3/2))
+var majorChord = channel(_ => C(1,5/4,3/2))
+var offHarmonics = channel(_ => H(1,1.2,1.5,3,7))
 
-// Strange wave
-//;[1,1.2,1.5,3,7].map(h).forEach(o => o.start())
+function N(scale = 1, seconds = 1) {
+  var o = O(440 * scale)
+  return {
+    seconds
+    ,start(t = ctx.currentTime) {
+      o.start(t)
+      o.stop(t + seconds) }
+    ,stop(t = ctx.currentTime) { o.stop(t) }}}
 
-function arp(freqs, duration = 1000) {
-  var os = []
-  freqs.forEach(f => os.push(h(f), h(3.05*f), h(5.1*f)))
-  return {duration
-          ,start() { os.forEach(o => o.start()) }
-          ,stop()  { os.forEach(o => o.stop()) }}}
+var playC = channel(_ => N(1))
 
-function silence(duration) {
-  return {duration, start: () => {}, stop: () => {}}}
-
-function play(/* notes */) {
+function ARP(/* notes */) {
   var notes = Array.from(arguments)
-  if (notes.length > 0) {
-    var [n, ...rest] = notes
-    n.start()
-    setTimeout(() => { n.stop(); play(...rest) }, n.duration)}}
+  var sounds = notes.map
+  return {
+    start() {
+      var t = ctx.currentTime
+      notes.forEach((n, i) => {
+        notes[i].start(t)
+        t += notes[i].seconds || 1
+        notes[i].stop(t) })}
+    ,stop() {
+      notes.forEach(n => n.stop())}}}
 
-// Chord progression
-play(arp([1, 1.2, 1.5])
-     ,arp([1, 1.4, 1.7])
-     ,arp([1, 1.5, 2]))
+var playArp = channel(_ => ARP(N(1), N(5/4), N(3/2)))
+
+var playScale = channel(_ => ARP(N(1), N(9/8), N(5/4), N(4/3), N(3/2), N(5/3), N(15/8), N(2)))
+
+function J(/* fundamentals */) {
+  var fundamentals = Array.from(arguments)
+  return (freq) => {
+    var waves = fundamentals.map(f => O(440 * freq * f, 1 / f))
+    return {
+      start(t = ctx.currentTime) {waves.forEach(w => w.start(t))}
+      ,stop(t = ctx.currentTime) {waves.forEach(w => w.stop(t))}}}}
+
+
+var playSquareArp = channel(_ => {
+  var s = J(1,3,5,7,9,11,13)
+  return ARP(s(1), s(5/4), s(3/2)) })
+
+var playSawArp = channel(_ => {
+  var s = J(1,2,3,4,5,6,7,8,9,10)
+  return ARP(s(1), s(5/4), s(3/2)) })
+
+var monotonousScale = channel(_ => ARP(N(1),
+                                       N(1 + 1/12),
+                                       N(1 + 2/12),
+                                       N(1 + 3/12),
+                                       N(1 + 4/12),
+                                       N(1 + 5/12),
+                                       N(1 + 6/12),
+                                       N(1 + 7/12),
+                                       N(1 + 8/12),
+                                       N(1 + 9/12),
+                                       N(1 + 10/12),
+                                       N(1 + 11/12),
+                                       N(1 + 12/12)))
+
+function S(seconds) {
+  return {seconds, start(){}, stop(){}}}
+
+var theme = channel(_ => ARP(N(1 + 8/12, 0.5), N(1 + 7/12, 0.25), N(1 + 6/12, 0.25),
+                             S(0.25),
+                             N(1 + 7/12, 0.25), N(1 + 8/12, 2)))
