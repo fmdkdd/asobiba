fb210a0aec3c54f41ebe4b7a6ac7fcc935cf1936
fmdkdd <fmdkdd@gmail.com>
2015-04-21 22:47:36 +0200

    Started exercises in chapter 4

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,1 +1,3 @@
 scalaVersion := "2.11.6"
+
+mainClass in (Compile, run) := Option( "ErrorTest" )
---
+++ scala/errorhandling.scala
@@ -0,0 +1,42 @@
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B >: A](ob: Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _ => None
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+}
+
+object ErrorTest extends App {
+}
