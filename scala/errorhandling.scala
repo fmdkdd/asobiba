87b2a7fa76d17f15abe3add4cf25c88eaf8f53c3
fmdkdd <fmdkdd@gmail.com>
2015-04-23 18:12:44 +0200

    Suspension got eaten by a grue

--- scala/errorhandling.scala
+++ scala/errorhandling.scala
@@ -9,7 +9,7 @@ sealed trait Option[+A] {
     case Some(a) => f(a)
   }
 
-  def getOrElse[B >: A](default: B): B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
