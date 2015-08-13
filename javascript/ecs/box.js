d6084f67d2eeb72c17286ccd42b5521c40e293c9
fmdkdd <fmdkdd@gmail.com>
2015-08-13 22:05:57 +0200

    Add convex polygon collision detection
    Using SAT theorem

--- javascript/ecs/box.js
+++ javascript/ecs/box.js
@@ -48,10 +48,134 @@ function is_point_inside_box(point, box) {
  */
 function do_boxes_collide(box1, box2) {
   // Consequence of the Separation Axis Theorem (SAT): if the two boxes overlap,
-  // their projections on the two axis overlap as well.  Conversely, if there is
+  // their projections on the two axes overlap as well.  Conversely, if there is
   // a gap in one axis, then the boxes do not overlap.
   return box1.x <= box2.x + box2.width && box1.x + box1.width >= box2.x
       && box1.y <= box2.y + box2.height && box1.y + box1.height >= box2.y
+}
+
+/**
+ * Return true if convex polygons POLY1 and POLY2 overlap.  Both arguments are
+ * arrays of vertices describing the polygons.  The array of vertices is open
+ * (i.e., there are as many points in the array as there are sides to the
+ * polygon).
+ */
+function do_polygons_collide(poly1, poly2) {
+  // Use the SAT theorem for determining if the two convex polygons collide.
+  return no_separation_axis(poly1, poly2) && no_separation_axis(poly2, poly1)
+}
+
+/**
+ * Return true if there is no separation axis between POLY1 and POLY2, checking
+ * only for the axes of POLY1.
+ *
+ * Used by do_polygons_collide.
+ */
+function no_separation_axis(poly1, poly2) {
+  for (var axis of get_axes(poly1)) {
+    var p1 = project(poly1, axis)
+    var p2 = project(poly2, axis)
+    if (!overlap(p1, p2))
+      return false
+  }
+
+  return true
+}
+
+/**
+ * Return the list of axes of POLY used to find a separation axis by
+ * no_separation_axis.  The list is returned as an array of vectors.
+ */
+function get_axes(poly) {
+  // For each edge of the polygon, return its normal.
+
+  var axes = []
+  for (var i = 0; i < poly.length; ++i) {
+    var p0 = poly[i]
+    var p1 = poly[(i + 1) % poly.length]
+    var edge = vec_minus(p0, p1)
+    var normal = vec_unit(vec_perp(edge))
+    // We return the normalized axis, though it is only necessary if we wish to
+    // find the minimum translation between the polygons.
+    axes.push(normal)
+  }
+
+  return axes
+}
+
+/**
+ * Return the interval obtained by projecting POLY onto AXIS.
+ * The interval is an object {min, max}.
+ */
+function project(poly, axis) {
+  // To project the polygon onto the axis, dot product each vertex with the
+  // axis, and keep the min and max values.
+
+  var min = +Infinity
+  var max = -Infinity
+
+  for (var vert of poly) {
+    var p = vec_dot(axis, vert)
+    if (p < min) min = p
+    if (p > max) max = p
+  }
+
+  return {min, max}
+}
+
+/**
+ * Return true if projections PROJ1 and PROJ2 overlap.
+ */
+function overlap(proj1, proj2) {
+  // Compare bounds of interval.  Single axis version of do_boxes_collide.
+  return proj1.min <= proj2.max && proj1.max >= proj2.min
+}
+
+function vec_length(v) {
+  return Math.sqrt(v.x*v.x + v.y*v.y)
+}
+
+function vec_unit(v) {
+  var l = vec_length(v)
+  return point(v.x / l, v.y / l)
+}
+
+function vec_perp(v) {
+  return point(-v.y, v.x)
+}
+
+function vec_plus(u, v) {
+  return point(u.x + v.x, u.y + v.y)
+}
+
+function vec_minus(u, v) {
+  return point(u.x - v.x, u.y - v.y)
+}
+
+function vec_dot(u, v) {
+  return u.x * v.x + u.y * v.y
+}
+
+function vec_rotate(v, a) {
+  var cos = Math.cos(a)
+  var sin = Math.sin(a)
+  return point(v.x * cos - v.y * sin,
+               v.x * sin + v.y * cos)
+}
+
+/**
+ * Rotate POLY along ANGLE, then translate it along VEC, and return the result
+ * as a new polygon.
+ *
+ * Useful for projecting hitboxes of rotating, moving objects which store their
+ * hitboxes as relative coordinates.
+ */
+function adjust_hitbox(poly, vec, angle) {
+  var p = []
+  for (var v of poly) {
+    p.push(vec_plus(vec_rotate(v, angle), vec))
+  }
+  return p
 }
 
 
