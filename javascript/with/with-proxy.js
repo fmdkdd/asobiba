dadab41699531c60b2bb9004312115ed2c014b53
fmdkdd <fmdkdd@gmail.com>
2015-08-18 11:47:46 +0200

    Doc, use SpiderMonkey

--- javascript/with/with-proxy.js
+++ javascript/with/with-proxy.js
@@ -2,14 +2,14 @@ var o = {a: 1};
 
 var h = {
 	getPropertyDescriptor: function(name) {
-		console.log('gpd', name);
+		print('gpd', name);
 		return Object.getOwnPropertyDescriptor(o, name);
 	},
 
 	counter: 0,
 
 	get: function(receiver, name) {
-		console.log(++this.counter, 'access to a');
+		print(++this.counter, 'access to a');
 		return o.a;
 	},
 };
@@ -17,5 +17,5 @@ var h = {
 var p = Proxy.create(h, o);
 
 with(p) {
-	console.log(a);
+	print(a);
 }
