6a503fecfc75d641d0778f95db98c2d3797228af
fmdkdd <fmdkdd@gmail.com>
2015-12-13 03:16:53 +0100

    Do not barf on test

--- rust/gbs/src/main.rs
+++ rust/gbs/src/main.rs
@@ -9,16 +9,17 @@ fn main() {
   let gbs = gbs::load(filename)
     .expect("Error loading GBS file");
 
-  println!("{:?}", gbs);
-  println!("load_addr: {:x}", gbs.load_addr);
-  println!("init_addr: {:x}", gbs.init_addr);
-  println!("play_addr: {:x}", gbs.play_addr);
-  println!("sp: {:x}", gbs.sp);
+  // println!("{:?}", gbs);
+  // println!("load_addr: {:x}", gbs.load_addr);
+  // println!("init_addr: {:x}", gbs.init_addr);
+  // println!("play_addr: {:x}", gbs.play_addr);
+  // println!("sp: {:x}", gbs.sp);
 
-  // println!("version: {}", gbs.version);
-  // println!("n_songs: {}", gbs.n_songs);
+  println!("version: {}", gbs.version);
+  println!("n_songs: {}", gbs.n_songs);
   // println!("first_song: {}", gbs.first_song);
-  // println!("title: {}", gbs.title);
-  // println!("author: {}", gbs.author);
-  // println!("copyright: {}", gbs.copyright);
+  println!("title: {}", gbs.title);
+  println!("author: {}", gbs.author);
+  println!("copyright: {}", gbs.copyright);
+  println!("rom len: {:x}", gbs.rom.len());
 }
