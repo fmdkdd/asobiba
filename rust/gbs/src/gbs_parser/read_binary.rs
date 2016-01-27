c5ad36020e511bbca89bc7e41f5ff8f634b5aa98
fmdkdd <fmdkdd@gmail.com>
2016-01-27 21:54:20 +0100

    read_exact landed in Rust 1.6

--- rust/gbs/src/gbs_parser/read_binary.rs
+++ rust/gbs/src/gbs_parser/read_binary.rs
@@ -1,35 +1,17 @@
-use std::io::{self, BufReader, Error, ErrorKind, Read};
+use std::io::{self, BufReader, Read};
 
 // Provide the following methods on types implementing Read.
 pub trait ReadBinary : Read {
 
-  // Backported from unstable read_exact in 1.5.0 source.
-  fn read_just(&mut self, mut buf: &mut [u8]) -> io::Result<()> {
-    while !buf.is_empty() {
-      match self.read(buf) {
-        Ok(0) => break,
-        Ok(n) => { let tmp = buf; buf = &mut tmp[n..]; }
-        Err(ref e) if e.kind() == ErrorKind::Interrupted => {}
-        Err(e) => return Err(e),
-      }
-    }
-    if !buf.is_empty() {
-      Err(Error::new(ErrorKind::Other,
-                     "failed to fill whole buffer"))
-    } else {
-      Ok(())
-    }
-  }
-
   fn read_u8(&mut self) -> io::Result<u8> {
     let mut buf = [0];
-    try!(self.read_just(&mut buf));
+    try!(self.read_exact(&mut buf));
     Ok(buf[0])
   }
 
   fn read_u16_le(&mut self) -> io::Result<u16> {
     let mut buf = [0, 0];
-    try!(self.read_just(&mut buf));
+    try!(self.read_exact(&mut buf));
     Ok(((buf[1] as u16) << 8) | (buf[0] as u16))
   }
 
