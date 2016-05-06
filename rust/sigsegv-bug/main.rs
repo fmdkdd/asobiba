958b9cf878699055a42904888639f45afeff845d
fmdkdd <fmdkdd@gmail.com>
2016-05-06 22:00:07 +0200

    Minimal example of SIGSEGV bug (dupe)

---
+++ rust/sigsegv-bug/main.rs
@@ -0,0 +1,46 @@
+pub const A_SIZE : usize = 0x10000;
+
+pub struct S {
+  a: [u8; A_SIZE],
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_ld() {
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+    S { a: [0; A_SIZE] };
+  }
+}
