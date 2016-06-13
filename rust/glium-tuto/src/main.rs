c66d4f8551f9421e200bcd4f030e837f93d2b62c
fmdkdd <fmdkdd@gmail.com>
2016-06-13 20:05:57 +0200

    Add Glium tuto

---
+++ rust/glium-tuto/src/main.rs
@@ -0,0 +1,89 @@
+// Glium test following the tutorial at
+// https://tomaka.github.io/glium/book/index.html
+
+#[macro_use]
+extern crate glium;
+
+use glium::{DisplayBuild, Surface};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+  position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+fn main() {
+  let display = glium::glutin::WindowBuilder::new()
+    .build_glium().unwrap();
+
+  let vertex_shader_src = r#"
+      #version 140
+
+      in vec2 position;
+      out vec2 my_attr;
+
+      uniform mat4 matrix;
+
+      void main() {
+        my_attr = position;
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+      }
+  "#;
+
+  let fragment_shader_src = r#"
+    #version 140
+
+    in vec2 my_attr;
+    out vec4 color;
+
+    void main() {
+      color = vec4(my_attr, 0.0, 1.0);
+    }
+  "#;
+
+  let program = glium::Program::from_source(
+    &display, vertex_shader_src, fragment_shader_src, None)
+    .unwrap();
+
+  let vertex1 = Vertex{ position: [-0.5, -0.5] };
+  let vertex2 = Vertex{ position: [ 0.0,  0.5] };
+  let vertex3 = Vertex{ position: [ 0.5, -0.25] };
+  let shape = vec![vertex1, vertex2, vertex3];
+  let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+  let mut t: f32 = -0.5;
+
+  loop {
+    t += 0.002;
+    if t > 0.5 {
+      t = -0.5;
+    }
+
+    let uniforms = uniform! {
+      matrix: [
+        [ t.cos(), t.sin(), 0.0, 0.0],
+        [-t.sin(), t.cos(), 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0f32],
+      ]
+    };
+
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 1.0, 1.0);
+    target.draw(&vertex_buffer, &indices, &program, &uniforms,
+                &Default::default()).unwrap();
+    target.finish().unwrap();
+
+
+    for ev in display.poll_events() {
+      match ev {
+        glium::glutin::Event::Closed => return,
+
+        _ => ()
+      }
+    }
+  }
+}
