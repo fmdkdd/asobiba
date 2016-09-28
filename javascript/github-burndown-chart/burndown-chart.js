8b5b7237ddcf2cec35e72c4f30f746c21648c69a
fmdkdd <fmdkdd@gmail.com>
2016-09-28 23:08:04 +0200

    Add file input to HTML page

--- javascript/github-burndown-chart/burndown-chart.js
+++ javascript/github-burndown-chart/burndown-chart.js
@@ -1,6 +1,17 @@
-document.addEventListener('DOMContentLoaded', init)
+document.addEventListener('DOMContentLoaded', load_data)
 
-function init() {
+function load_data() {
+  var file = document.querySelector('#file-input').files[0]
+
+  var reader = new FileReader()
+  reader.onload = function(e) { redraw_graphs(e.target.result) }
+  reader.onerror = function(err) { console.error(err) }
+  reader.readAsText(file)
+}
+
+function redraw_graphs(data) {
+  d3.selectAll("svg").remove()
+
   var margin = {top: 20, right: 40, bottom: 40, left: 20 }
   // Width and height of the graph, not of the SVG
   var width = 900 - margin.left - margin.right
@@ -15,97 +26,90 @@ function init() {
   // Nice colors provided by d3
   var c = d3.schemeCategory20
 
-  d3.json("spacemacs-issues.json", function getData(err, data) {
-    if (err) {
-      console.error(err)
-      return
-    }
+  var [points, total_issues, closed_issues] = prepare_data(JSON.parse(data))
+  var earliest = points[0].time
 
-    var [points, total_issues, closed_issues] = prepare_data(data)
-    var earliest = points[0].time
+  // X and Y axes
+  var x = d3.scaleTime()
+      .domain([earliest, Date.now()])
+      .range([0, width])
 
-    // X and Y axes
-    var x = d3.scaleTime()
-        .domain([earliest, Date.now()])
-        .range([0, width])
-
-    var y = d3.scaleLinear()
-        .domain([0, total_issues])
-        .range([height, 0])   // reversed because SVG origin is top-left
-
-    // Draw the axes
-    svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x))
-
-    svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", `translate(${width}, 0)`)
-      .call(d3.axisRight(y))
-
-    // Draw the total (open+closed) area
-    var total_issues_area = d3.area()
-        .x(function(d) { return x(d.time) })
-        .y1(function(d) { return y(d.total) })
-        .y0(y(0))
-
-    svg.datum(points)
-      .append("path")
-      .attr("d", total_issues_area)
-      .attr("fill", c[7])
-
-    // Draw the closed area
-    var closed_issues_area = d3.area()
-        .x(function(d) { return x(d.time) })
-        .y1(function(d) { return y(d.closed) })
-        .y0(y(0))
-
-    svg.datum(points)
-      .append("path")
-      .attr("d", closed_issues_area)
-      .attr("fill", c[0])
-
-    // Second SVG that only displays the running total of opened issues
-    // (open+closed - closed)
-
-    // Adjust height
-    height = 200 - margin.top - margin.bottom
-
-    // Create SVG
-    var open_issues_svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`)
-
-    // Change Y axis
-    y = d3.scaleLinear()
-      .domain([0, total_issues - closed_issues])
+  var y = d3.scaleLinear()
+      .domain([0, total_issues])
       .range([height, 0])   // reversed because SVG origin is top-left
 
-    // Draw the axes
-    open_issues_svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x))
+  // Draw the axes
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
 
-    open_issues_svg.append("g")
-      .attr("class", "y axis")
-      .attr("transform", `translate(${width}, 0)`)
-      .call(d3.axisRight(y).ticks(4))
+  svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", `translate(${width}, 0)`)
+    .call(d3.axisRight(y))
 
-    // Draw the area of number of open issues over time
-    var open_issues_area = d3.area()
-        .x(function(d) { return x(d.time) })
-        .y1(function(d) { return y(d.total - d.closed) })
-        .y0(y(0))
+  // Draw the total (open+closed) area
+  var total_issues_area = d3.area()
+      .x(function(d) { return x(d.time) })
+      .y1(function(d) { return y(d.total) })
+    .y0(y(0))
 
-    open_issues_svg.datum(points)
-      .append("path")
-      .attr("d", open_issues_area)
-      .attr("fill", c[7])
-  })
+  svg.datum(points)
+    .append("path")
+      .attr("d", total_issues_area)
+    .attr("fill", c[7])
+
+  // Draw the closed area
+  var closed_issues_area = d3.area()
+    .x(function(d) { return x(d.time) })
+    .y1(function(d) { return y(d.closed) })
+    .y0(y(0))
+
+  svg.datum(points)
+    .append("path")
+    .attr("d", closed_issues_area)
+    .attr("fill", c[0])
+
+  // Second SVG that only displays the running total of opened issues
+  // (open+closed - closed)
+
+  // Adjust height
+  height = 200 - margin.top - margin.bottom
+
+  // Create SVG
+  var open_issues_svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+  // Change Y axis
+  y = d3.scaleLinear()
+    .domain([0, total_issues - closed_issues])
+    .range([height, 0])   // reversed because SVG origin is top-left
+
+  // Draw the axes
+  open_issues_svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
+
+  open_issues_svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", `translate(${width}, 0)`)
+    .call(d3.axisRight(y).ticks(4))
+
+  // Draw the area of number of open issues over time
+  var open_issues_area = d3.area()
+    .x(function(d) { return x(d.time) })
+    .y1(function(d) { return y(d.total - d.closed) })
+    .y0(y(0))
+
+  open_issues_svg.datum(points)
+    .append("path")
+    .attr("d", open_issues_area)
+    .attr("fill", c[7])
 }
 
 function prepare_data(issues) {
