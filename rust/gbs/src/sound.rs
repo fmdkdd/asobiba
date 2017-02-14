9c588b6537af31a5130261b516747afa210ef7a7
fmdkdd <fmdkdd@gmail.com>
2017-02-14 17:40:53 +0100

    More sound

--- rust/gbs/src/sound.rs
+++ rust/gbs/src/sound.rs
@@ -1,3 +1,36 @@
+use time::Duration;
+
+struct Counter {
+  enabled: bool,
+  counter: u8,
+}
+
+impl Counter {
+  pub fn is_enabled(&self) -> bool {
+    enabled
+  }
+
+  pub fn reload(&mut self, value: u8) {
+    self.counter = value;
+  }
+
+  pub fn trigger(&mut self) {
+    self.enabled = true;
+    if self.counter == 0 {
+      self.counter = 64;
+    }
+  }
+
+  pub fn clock(&mut self) {
+    if self.enabled && self.counter > 0 {
+      self.counter -= 1;
+      if self.counter == 0 {
+        self.enabled = false;
+      }
+    }
+  }
+}
+
 enum SweepDirection {
   DECREASE, INCREASE
 }
@@ -9,26 +42,38 @@ enum ShiftRegisterWidth {
 // TODO: most of these are not 8 bit numbers
 struct Square1 {
   // Sweep register
-  sweep_period: u8,
-  sweep_direction: SweepDirection,
-  sweep_shift: u8,
+  // sweep_period: u8,
+  // sweep_direction: SweepDirection,
+  // sweep_shift: u8,
 
-  wave_pattern_duty: u8,
-  length_load: u8,
+  // wave_pattern_duty: u8,
+  // length_load: u8,
 
-  initial_volume: u8,
-  volume_sweep_direction: SweepDirection,
-  sweep_length: u8,
+  // initial_volume: u8,
+  // volume_sweep_direction: SweepDirection,
+  // sweep_length: u8,
 
-  frequency: u16,
-  shift_clock_freq: u8,
-  shift_register_width: ShiftRegisterWidth,
-  freq_dividing_ratio: u8,
+  frequency_period: u16,
+  // shift_clock_freq: u8,
+  // shift_register_width: ShiftRegisterWidth,
+  // freq_dividing_ratio: u8,
+}
 
-  length_counter_enable: bool,
-  hh
-
-
+impl Square1 {
 
 
+  pub fn set_frequency(&mut self, freq: u16) {
+    self.frequency_period = (2048 - freq) * 4;
+  }
+
+  pub fn set_length_enable(&mut self ) {
+  }
+
+  pub fn clock(&mut self) -> Vec<u8> {
+
+  }
+
+  pub fn run(&mut self, dt: Duration) {
+
+  }
 }
