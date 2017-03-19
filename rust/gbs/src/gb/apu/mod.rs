2004b7fccf165ddf0cba36075f01ece9465cf7e4
fmdkdd <fmdkdd@gmail.com>
2017-03-19 15:01:06 +0100

    Split APU into modules

--- rust/gbs/src/gb/apu/mod.rs
+++ rust/gbs/src/gb/apu/mod.rs
@@ -1,139 +1,26 @@
+mod pulse;
+
+use gb::apu::pulse::Pulse;
+
 use std::io::Write;
 
-const DUTY_WAVEFORMS : [[u8; 8]; 4] = [
-  [0,0,0,0,0,0,0,1],
-  [1,0,0,0,0,0,0,1],
-  [1,0,0,0,0,1,1,1],
-  [0,1,1,1,1,1,1,0],
-];
-
-#[derive(Copy,Clone)]
-enum Duty {
-  HalfQuarter,
-  Quarter,
-  Half,
-  ThreeQuarters,
-}
-
-impl Duty {
-  // Can't use From trait as this conversion can fail
-  fn from_u8(v: u8) -> Option<Self> {
-    match v {
-      0 => Some(Duty::HalfQuarter),
-      1 => Some(Duty::Quarter),
-      2 => Some(Duty::Half),
-      3 => Some(Duty::ThreeQuarters),
-      _ => None,
-    }
-  }
-}
-
-enum Sweep {
-  Decrease,
-  Increase,
-}
-
-impl Sweep {
-  fn from_u8(v: u8) -> Option<Self> {
-    match v {
-      0 => Some(Sweep::Decrease),
-      1 => Some(Sweep::Increase),
-      _ => None,
-    }
-  }
-}
-
-struct Pulse {
-  enabled: bool,
-
-  period: u16,
-  frequency: u16,
-  duty: Duty,
-  duty_idx: u8,
-
-  length_counter: u8,
-
-  volume: u8,
-  volume_init: u8,
-  volume_counter: u8,
-  volume_period: u8,
-  volume_sweep: Sweep,
-}
-
-impl Pulse {
-  fn new() -> Self {
-    Pulse {
-      enabled: false,
-      period: 0,
-      frequency: 0,
-      duty: Duty::Half,
-      duty_idx: 0,
-      length_counter: 0,
-      volume: 0,
-      volume_init: 0,
-      volume_counter: 0,
-      volume_period: 0,
-      volume_sweep: Sweep::Decrease,
-    }
-  }
-
-  fn trigger(&mut self) {
-    self.enabled = true;
-    if self.length_counter == 0 {
-      self.length_counter = 64;
-    }
-    self.period = (2048 - self.frequency) * 4;
-    self.volume_counter = self.volume_period;
-    self.volume = self.volume_init;
-  }
-
-  fn clock_length(&mut self) {
-    if self.length_counter > 0 {
-      self.length_counter -= 1;
-    } else {
-      self.enabled = false;
-    }
-  }
-
-  fn clock_envelope(&mut self) {
-    if self.volume_period > 0 {
-      if self.volume_counter > 0 {
-        self.volume_counter -= 1;
-      } else {
-        let new_volume = match self.volume_sweep {
-          Sweep::Decrease => self.volume.wrapping_sub(1),
-          Sweep::Increase => self.volume + 1,
-        };
-
-        if new_volume <= 15 {
-          self.volume = new_volume;
-          self.volume_counter = self.volume_period;
-        }
-      }
-    }
-  }
-
-  fn clock_frequency(&mut self) {
-    if self.period > 0 {
-      self.period -= 1;
-    } else {
-      self.period = (2048 - self.frequency) * 4;
-      self.duty_idx = (self.duty_idx + 1) % 8;
-    }
-  }
-
-  fn output(&self) -> u8 {
-    if self.enabled {
-      DUTY_WAVEFORMS[self.duty as usize][self.duty_idx as usize]
-        * self.volume
-    } else {
-      0
-    }
-  }
+pub enum Register {
+  NR10,
+  NR11,
+  NR12,
+  NR13,
+  NR14,
+  NR21,
+  NR22,
+  NR23,
+  NR24,
 }
 
 pub struct APU {
   pulse1: Pulse,
+
+  // TODO: read/write and clock pulse2
+  pulse2: Pulse,
 
   frame_seq: FrameSequencer,
 
@@ -151,6 +38,7 @@ impl APU {
   pub fn new() -> Self {
     APU {
       pulse1: Pulse::new(),
+      pulse2: Pulse::new(),
 
       frame_seq: FrameSequencer::new(),
 
@@ -166,9 +54,10 @@ impl APU {
   }
 
   pub fn read(&self, addr: u16) -> u8 {
+    use self::Register::*;
+
     match addr {
-      0xFF14 => (if self.pulse1.enabled { 1 } else { 0 } << 6)
-        | 0xBF,
+      0xFF14 => self.pulse1.read(NR14),
 
       0xFF25 => (if self.right_enable_ch1 { 1 } else { 0 })
         | (if self.right_enable_ch2 { 1 } else { 0 } << 1)
@@ -184,34 +73,21 @@ impl APU {
   }
 
   pub fn write(&mut self, addr: u16, w: u8) {
+    use self::Register::*;
+
     writeln!(&mut ::std::io::stderr(), "poke {:x} {:x}", addr, w).unwrap();
 
     match addr {
-      0xFF11 => {
-        self.pulse1.duty = Duty::from_u8(w >> 6).unwrap();
-        self.pulse1.length_counter = 64 - (w & 0x3F);
-      },
+      0xFF10 => self.pulse1.write(NR10, w),
+      0xFF11 => self.pulse1.write(NR11, w),
+      0xFF12 => self.pulse1.write(NR12, w),
+      0xFF13 => self.pulse1.write(NR13, w),
+      0xFF14 => self.pulse1.write(NR14, w),
 
-      0xFF12 => {
-        self.pulse1.volume_init = w >> 4;
-        self.pulse1.volume_sweep = Sweep::from_u8((w >> 3) & 0x1).unwrap();
-        self.pulse1.volume_period = w & 0x7;
-      }
-
-      0xFF13 => {
-        self.pulse1.frequency = (self.pulse1.frequency & 0x0700)
-          | w as u16;
-      },
-
-      0xFF14 => {
-        self.pulse1.frequency = (self.pulse1.frequency & 0xFF)
-          | ((w & 0x7) as u16) << 8;
-        self.pulse1.enabled = (w & 0x40) > 0;
-
-        if w & 0x80 > 0 {
-          self.pulse1.trigger();
-        }
-      }
+      0xFF16 => self.pulse2.write(NR21, w),
+      0xFF17 => self.pulse2.write(NR22, w),
+      0xFF18 => self.pulse2.write(NR23, w),
+      0xFF19 => self.pulse2.write(NR24, w),
 
       0xFF25 => {
         self.right_enable_ch1 = (w & 0x01) > 0;
@@ -271,6 +147,7 @@ impl APU {
   }
 
   fn clock_128(&mut self) {
+    self.pulse1.clock_sweep();
   }
 
   fn clock_64(&mut self) {
