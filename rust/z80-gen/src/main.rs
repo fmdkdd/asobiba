ae374358ae640a4c65cf7e453ee92fdfc153b158
fmdkdd <fmdkdd@0xc0de.fr>
2018-06-28 17:09:29 +0200

    Generating ADD and LD instructions

--- rust/z80-gen/src/main.rs
+++ rust/z80-gen/src/main.rs
@@ -7,15 +7,18 @@
 #[macro_use] extern crate serde_derive;
 extern crate docopt;
 extern crate regex;
+extern crate strfmt;
 
 mod parser;
 
 use docopt::Docopt;
+use std::collections::{HashMap, HashSet};
 use std::io::{BufRead, BufReader, BufWriter};
 use std::fs::File;
 use std::str::FromStr;
+use strfmt::strfmt;
 
-use parser::{ParsedOpcode, ParsedOperand};
+use parser::{Mnemonic, ParsedOpcode, ParsedOperand};
 
 #[derive(Debug)]
 enum ArgType {
@@ -46,7 +49,7 @@ fn emit_disassembler_arg(arg: &ParsedOperand) -> (String, ArgType) {
   }
 }
 
-fn emit_disassembler<W>(ops: &[ParsedOpcode], mut writer: W) where W : std::io::Write {
+fn emit_disassembler<W>(ops: &[ParsedOpcode], writer: &mut W) where W : std::io::Write {
   // For each opcode, we emit a struct that gives the necessary data for a
   // common function to produce a disassembly string
   for o in ops {
@@ -71,30 +74,172 @@ fn emit_disassembler<W>(ops: &[ParsedOpcode], mut writer: W) where W : std::io::
   }
 }
 
-fn emit_op_table<W>(ops: &[ParsedOpcode], mut writer: W) where W : std::io::Write {
-  for o in ops {
-    writeln!(writer, "[0x{:04x}] = z80_op_nop,", o.pattern.code).unwrap();
+// Used for function name
+fn emit_full_arg(arg: &Option<ParsedOperand>) -> String {
+  use ParsedOperand::*;
+
+  match arg {
+    &Some(Bit(b)) => format!("b_{}", b),
+    &Some(Register(ref src)) => format!("{}", src).to_lowercase(),
+    &Some(Immediate) => format!("n"),
+    &Some(ImmediateExtended) => format!("nn"),
+    &Some(ZeroPage(n)) => format!("z_{}", n),
+    &Some(AddressRelative) => format!("reladd"),
+    &Some(AddressImmediate) => format!("immadd"),
+    &Some(AddressExtended) => format!("extadd"),
+    &Some(AddressIndexed(ref src)) => format!("{}_d", src).to_lowercase(),
+    &Some(AddressRegister(ref src)) => format!("{}_ind", src).to_lowercase(),
+    &Some(Conditional(ref cond)) => format!("{:?}", cond).to_lowercase(),
+
+    &None => "".to_string(),
+
+    _ => unreachable!(),
   }
+}
+
+// Used for reading the operand
+fn emit_src_arg(arg: &Option<ParsedOperand>) -> String {
+  use ParsedOperand::*;
+
+  let decl = "uint8_t src =";
+
+  match arg {
+    &Some(Bit(b)) => format!("{} {}", decl, b),
+    &Some(Register(ref src)) => format!("{} z->{}", decl, src).to_lowercase(),
+    &Some(Immediate) => format!("{} z->pc++", decl),
+    &Some(ImmediateExtended) => format!(r#"uint16_t lo = z->pc++;
+uint16_t hi = z->pc++;
+{} hi << 8 | lo;"#, decl),
+    &Some(ZeroPage(n)) => format!("{} = z->ram[{}]", decl, n),
+    &Some(AddressRelative) => format!(r#"int8_t dis = z->pc++;
+{} z->ram[z->pc + dis]"#, decl),
+    &Some(AddressImmediate) => format!("{} = z->ram[z->pc++]", decl),
+    &Some(AddressExtended) => format!(r#"uint16_t lo = z->pc++;
+uint16_t hi = z->pc++;
+{} z->ram[hi << 8 | lo]"#, decl),
+    &Some(AddressIndexed(ref src)) => format!("{} z->ram[z->{} + (int8_t) z->pc++]",
+                                              decl, src).to_lowercase(),
+    &Some(AddressRegister(ref src)) => format!("{} z->{}", decl, src).to_lowercase(),
+    &Some(Conditional(ref cond)) => format!("{} z->{:?}", decl, cond).to_lowercase(),
+
+    &None => "".to_string(),
+
+    _ => unreachable!(),
+  }
+}
+
+// Used for writing to the operand
+fn emit_dst_arg(arg: &Option<ParsedOperand>) -> String {
+  use ParsedOperand::*;
+
+  match arg {
+    &Some(Register(ref src)) => format!("z->{}", src).to_lowercase(),
+    &Some(ZeroPage(n)) => format!("z->ram[{}]", n),
+    &Some(AddressRelative) => format!(r#"int8_t dis = z->pc++;
+z->ram[z->pc + dis]"#),
+    &Some(AddressImmediate) => format!("z->ram[z->pc++]"),
+    &Some(AddressExtended) => format!(r#"uint16_t lo = z->pc++;
+uint16_t hi = z->pc++;
+z->ram[hi << 8 | lo]"#),
+    &Some(AddressIndexed(ref src)) => format!("z->ram[z->{} + (int8_t) z->pc++]", src).to_lowercase(),
+    &Some(AddressRegister(ref src)) => format!("z->ram[z->{}]", src).to_lowercase(),
+
+    &None => "".to_string(),
+
+    _ => {
+      println!("{:?}", arg);
+
+      unreachable!()
+    }
+  }
+}
+
+fn emit_op_table<W>(ops: &[ParsedOpcode], wop: &mut W, wfun: &mut W) where W : std::io::Write {
+  use Mnemonic::*;
+
+  let mut defined = HashSet::new();
+
+  for o in ops {
+    let mut fname = "";
+    let mut body = "";
+    let mut cycles = 1;
+    let mut not_yet = false;
+
+    match &o.name {
+      &ADD => {
+        fname = "z80_op_add_{dst}_{src}";
+        body = "{src};\n{dst} += src;"
+      }
+
+      &LD => {
+        fname = "z80_op_ld_{dst}_{src}";
+        body = "{src};\n{dst} = src;";
+      }
+
+      _ => not_yet = true
+    };
+
+    if !not_yet {
+      let mut full_vars = HashMap::new();
+      full_vars.insert("dst".to_string(), emit_full_arg(&o.dst));
+      full_vars.insert("src".to_string(), emit_full_arg(&o.src));
+      let fname_str = strfmt(fname, &full_vars).unwrap();
+
+      let mut vars = HashMap::new();
+      vars.insert("dst".to_string(), emit_dst_arg(&o.dst));
+      vars.insert("src".to_string(), emit_src_arg(&o.src));
+
+      // Write the specialized function definition
+      if !defined.contains(&fname_str) {
+        writeln!(wfun, r#"
+uint8_t {n}(Z80 *z) {{
+  {b}
+  return {c};
+}}"#,
+                 n = fname_str,
+                 b = strfmt(body, &vars).unwrap(),
+                 c = cycles).unwrap();
+
+        defined.insert(fname_str);
+      }
+
+      // And add it to the ops table
+      writeln!(wop, "[0x{:04x}] = {},",
+               o.pattern.code,
+               strfmt(fname, &full_vars).unwrap()).unwrap();
+    } else {
+       writeln!(wop, "[0x{:04x}] = z80_op_nop,", o.pattern.code).unwrap();
+    }
+  }
+}
+
+fn emit_funcs_header<W>(w: &mut W) where W : std::io::Write {
+  write!(w, r#"
+#pragma once
+
+#include <stdint.h>
+
+#include "z80.h"
+"#).unwrap();
 }
 
 const USAGE: &'static str = "
 A Z80 emulator code generator.
 
 Usage:
-  z80-gen -d <disassembly> -c <cpu> <opsfile>
+  z80-gen <opsfile> <disassemblytable> <opstable> <funcs>
   z80-gen -h
 
 Options:
   -h, --help              Show this help.
-  -d <disassembly>        Write the disassembly table to this file.
-  -c <cpu>                Write opcode tables to this file.
 ";
 
 #[derive(Deserialize)]
 struct Args {
   arg_opsfile: String,
-  flag_d: String,
-  flag_c: String,
+  arg_disassemblytable: String,
+  arg_opstable: String,
+  arg_funcs: String,
 }
 
 fn main() {
@@ -127,9 +272,19 @@ fn main() {
     //   println!("{:?}", o);
     // }
 
-    emit_disassembler(&good_ops, BufWriter::new(File::create(&args.flag_d)
-                                                .expect(&format!("Can't open {} file", &args.flag_d))));
-    emit_op_table(&good_ops, BufWriter::new(File::create(&args.flag_c)
-                                            .expect(&format!("Can't open {} file", &args.flag_c))));
+    let mut filename = &args.arg_disassemblytable;
+    emit_disassembler(&good_ops,
+                      &mut BufWriter::new(File::create(&filename)
+                                          .expect(&format!("Can't open {} file", &filename))));
+
+    filename = &args.arg_opstable;
+    let mut wops = BufWriter::new(File::create(&filename)
+                              .expect(&format!("Can't open {} file", &filename)));
+    filename = &args.arg_funcs;
+    let mut wfun = BufWriter::new(File::create(&filename)
+                              .expect(&format!("Can't open {} file", &filename)));
+    emit_funcs_header(&mut wfun);
+    emit_op_table(&good_ops, &mut wops, &mut wfun);
+
   }
 }
