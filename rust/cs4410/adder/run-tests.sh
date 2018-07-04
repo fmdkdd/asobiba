a7b8a9903ef5b16bfb49072d82ba9a19c0f3c30b
fmdkdd <fmdkdd@0xc0de.fr>
2018-07-04 23:03:43 +0200

    Exit run-tests on failure

--- rust/cs4410/adder/run-tests.sh
+++ rust/cs4410/adder/run-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Exit on first failure
+set -e
+
 # Make sure the compiler is up to date
 cargo build
 
@@ -9,14 +12,18 @@ make
 
 # Check the output of each binary against the expected output (the first line of
 # the adder file)
+ret=0
 echo == Running tests...
 for file in `find build/ -executable -type f`; do
-    bin=`basename $file`
-    expect=`head -n1 tests/$bin.adder | cut -d: -f2`
+    bin=`basename "$file"`
+    expect=`head -n1 "tests/$bin.adder" | cut -d: -f2`
     actual=`./$file`
     if [ $expect != $actual ]; then
-        echo FAIL: $bin: expected $expect, got $actual
+        echo FAIL: "$bin": expected $expect, got $actual
+        ret=1
     else
-        echo pass: $bin
+        echo pass: "$bin"
     fi
 done
+
+exit $ret
