b33864125df87c87bc72373e5215d3ade205356b
fmdkdd <fmdkdd@0xc0de.fr>
2018-07-17 22:37:04 +0200

    Split into files

--- rust/cs4410/boa/src/bin/debug.rs
+++ rust/cs4410/boa/src/bin/debug.rs
@@ -4,12 +4,12 @@ use std::io::{self, Read};
 
 use boa::lex::TokenStream;
 use boa::parse::parse;
-use boa::compile::compile;
+use boa::compile::debug;
 
 fn main() {
   let stdin = io::stdin();
   let mut input = String::new();
   stdin.lock().read_to_string(&mut input).unwrap();
   let ast = parse(&mut TokenStream::new(&input));
-  println!("{}", compile(ast));
+  debug(ast);
 }
