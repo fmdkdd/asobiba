418f6c4573d8e0e3aeb2c545dcfbaaaa656c6a13
fmdkdd <fmdkdd@0xc0de.fr>
2018-10-31 17:34:16 +0100

    Buggish gutter clicker
    But at least I can see dead code starting from any other line by
    clicking  in the gutter.

--- javascript/cfgpunks/script.js
+++ javascript/cfgpunks/script.js
@@ -1,3 +1,21 @@
+/**
+ * Prototype for an EXAPUNKS editor doing dead code analysis.
+ *
+ * Idea from
+ * https://old.reddit.com/r/exapunks/comments/9s0oxn/the_game_should_use_unreachable_code_detection/
+ *
+ * We use CodeMirror for the editor.  Whenever the code changes, we run the dead
+ * code analysis and highlight the dead code in light grey text.
+ *
+ * If the user clicks a REPL keyword, we show the dead code analysis from the
+ * point of view of the spawned EXA (i.e., starting from the REPL target label).
+ */
+
+// TODO: currently we need to click on the gutter to change the starting line,
+// but that is buggy if the document changes, as that will invalidate the line.
+// The less surprising UX is to always analyse from the marker, as long as the
+// marker is present, and remove the marker if we remove the line.
+
 document.addEventListener('DOMContentLoaded', init);
 
 // CodeMirror instance
@@ -10,21 +28,45 @@ function init() {
     lineNumbers: true,
   })
 
+  // Turn text to uppercase automatically
+  cm.on('beforeChange', (_, obj) => {
+    obj.update(null, null,
+               obj.text.map(l => l.toUpperCase()))
+  })
+
   // Save to text area on change lets the browser save the content of the area
   // without explicitly using local storage.
   cm.on('change', _ => cm.save())
 
+  // Trigger analysis on input change
   cm.on('change', debounce(analyze, 20))
-  analyze() // trigger analysis on load
+
+  // Whenever we click on the gutter, analyze code starting from this line from
+  // now on.
+  cm.on('gutterClick', (_, line) => {
+    highlight_lines([line], 'start-line', 'gutter')
+    analyze(line)
+  })
+
+  // Trigger analysis on load
+  analyze()
 }
 
-function analyze() {
+// Analyze code to find and highlight dead code starting from editor line START.
+function analyze(start = find_start_line()) {
   const code = parse(cm.getValue())
   // TODO: validate code (ensure labels are defined and unique)
-  const cfg = build_cfg(code)
+  const cfg = build_cfg(code, code_line(code, start))
   const dead = find_dead_code(code, cfg)
 
   highlight_dead_code(dead, code)
+}
+
+// Return the editor line of code the user clicked on, or the default starting
+// line of the program (0).
+function find_start_line() {
+  // TODO:
+  return 0
 }
 
 // Highlight dead code lines in the editor.
@@ -34,16 +76,29 @@ function highlight_dead_code(dead, code) {
   // Convert to editor lines
   const dead_lines = dead.map(l => code[l].line)
 
+  highlight_lines(dead_lines, 'dead')
+}
+
+// Add class CSS to lines LINES, and remove it on all the other lines in the
+// document.
+//
+// WHERE has the same meaning as in cm.addLineClass.
+function highlight_lines(lines, css, where = 'text') {
   cm.eachLine(l => {
-    if (dead_lines.includes(cm.getLineNumber(l))) {
-      cm.addLineClass(l, 'text', 'dead')
+    if (lines.includes(cm.getLineNumber(l))) {
+      cm.addLineClass(l, where, css)
     } else {
-       cm.removeLineClass(l, 'text', 'dead')
+      cm.removeLineClass(l, where, css)
     }
   })
 }
 
-// Return a control-flow graph (CFG) of CODE, starting at line START.
+// Return the index of instruction in CODE corresponding to editor LINE.
+function code_line(code, line) {
+  return code.findIndex(l => l.line === line)
+}
+
+// Return a control-flow graph (CFG) of CODE, starting at index START.
 //
 // The CFG is an array of successors, as indices into the CODE array.
 function build_cfg(code, start = 0) {
@@ -55,6 +110,17 @@ function build_cfg(code, start = 0) {
     switch (instr.keyword) {
     case 'JUMP':
       return [label_line(instr.args[0])]
+
+    case 'FJMP':
+    case 'TJMP':
+      const targets = [label_line(instr.args[0])]
+      if (n + 1 < code.length) {
+        targets.push(n + 1)
+      }
+      return targets
+
+    case 'HALT':
+      return []
 
     default:
       return n + 1 < code.length ? [n + 1] : []
