6e31f943fb880c0a4e8da821117dbf22b3ac96b1
fmdkdd <fmdkdd@0xc0de.fr>
2018-12-10 22:56:40 +0100

    Add color to run-tests

--- rust/cs4410/diamondback/run-tests.sh
+++ rust/cs4410/diamondback/run-tests.sh
@@ -3,49 +3,47 @@
 COMP=target/debug/diamondback
 SUFFIX=.diamondback
 
-# Return value
-ret=0
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
 # Make sure the compiler is up to date
 cargo build || exit 2
 
+# Build all the tests/*.diamondback files to build/* binaries
+echo -e "${YELLOW}== Building dynamic tests...${NC}"
+make || exit 3
+
+# Return value
+ret=0
+
 # Run static tests first
-echo == Running static tests...
+echo -e "${YELLOW}== Running static tests...${NC}"
 
 for file in `find tests/static/ -name "*${SUFFIX}" | sort`; do
     base=`basename -s $SUFFIX "$file"`
     expect=`head -n1 $file | cut -d: -f2-`
     actual=`$COMP < $file 2>&1 | rg :error: | cut -d: -f2-`
     if [[ "$expect" == "$actual" ]]; then
-        echo pass: "$base"
+        echo -e "${GREEN}pass${NC}:" "$base"
     else
-        echo FAIL: "$base": expected \"$expect\", got \"$actual\"
+        echo -e "${RED}FAIL${NC}:" "$base": expected \"$expect\", got \"$actual\"
         ret=1
     fi
 done
 
-# Exit early if any static test failed
-if [[ $ret != 0 ]]; then
-    exit $ret
-fi
-ret=0
-
-# Build all the tests/*.diamondback files to build/* binaries
-echo == Building dynamic tests...
-make || exit 3
-
-# Check the output of each binary against the expected output (the first line of
-# the adder file)
-ret=0
-echo == Running dynamic tests...
+# Check the output of each binary against the expected output (the first line)
+echo -e "${YELLOW}== Running dynamic tests...${NC}"
 for file in `find build/ -executable -type f | sort`; do
     bin=`basename "$file"`
     expect=`head -n1 "tests/$bin$SUFFIX" | cut -d: -f2-`
     actual=`./$file 2>&1`
     if [[ "$expect" == "$actual" ]]; then
-        echo pass: "$bin"
+        echo -e "${GREEN}pass${NC}:" "$bin"
     else
-        echo FAIL: "$bin": expected \"$expect\", got \"$actual\"
+        echo -e "${RED}FAIL${NC}:" "$bin": expected \"$expect\", got \"$actual\"
         ret=1
     fi
 done
