686824df1832bea2091a608bc87cd852fb6ab83f
fmdkdd <fmdkdd@0xc0de.fr>
2019-04-22 11:05:30 +0200

    Cleanup

--- asm/nes-tuto/main.s
+++ asm/nes-tuto/main.s
@@ -1,142 +1,140 @@
-;
-; iNES header
-;
+;;
+;; iNES header
+;;
 
 .segment "HEADER"
 
-INES_MAPPER = 0 ; 0 = NROM
-INES_MIRROR = 1 ; 0 = horizontal mirroring, 1 = vertical mirroring
-INES_SRAM   = 0 ; 1 = battery backed SRAM at $6000-7FFF
+INES_MAPPER = 0               ; 0 = NROM
+INES_MIRROR = 1               ; 0 = horizontal mirroring, 1 = vertical mirroring
+INES_SRAM   = 0               ; 1 = battery backed SRAM at $6000-7FFF
 
-.byte 'N', 'E', 'S', $1A ; ID
-.byte $02 ; 16k PRG chunk count
-.byte $01 ; 8k CHR chunk count
+.byte 'N', 'E', 'S', $1A        ; ID
+.byte $02                       ; 16k PRG chunk count
+.byte $01                       ; 8k CHR chunk count
 .byte INES_MIRROR | (INES_SRAM << 1) | ((INES_MAPPER & $f) << 4)
 .byte (INES_MAPPER & %11110000)
 .byte $0, $0, $0, $0, $0, $0, $0, $0 ; padding
 
-;
-; CHR ROM
-;
+;;
+;; CHR ROM
+;;
 
 .segment "TILES"
 .incbin "background.chr"
 .incbin "sprite.chr"
 
-;
-; vectors placed at top 6 bytes of memory area
-;
+;;
+;; vectors placed at top 6 bytes of memory area
+;;
 
 .segment "VECTORS"
 .word nmi
 .word reset
 .word irq
 
-;
-; reset routine
-;
+;;
+;; reset routine
+;;
 
 .segment "CODE"
 reset:
-	sei       ; mask interrupts
+	sei                     ; mask interrupts
 	lda #0
-	sta $2000 ; disable NMI
-	sta $2001 ; disable rendering
-	sta $4015 ; disable APU sound
-	sta $4010 ; disable DMC IRQ
+	sta $2000               ; disable NMI
+	sta $2001               ; disable rendering
+	sta $4015               ; disable APU sound
+	sta $4010               ; disable DMC IRQ
 	lda #$40
-	sta $4017 ; disable APU IRQ
-	cld       ; disable decimal mode
+	sta $4017               ; disable APU IRQ
+	cld                     ; disable decimal mode
 	ldx #$FF
-	txs       ; initialize stack
+	txs                     ; initialize stack
 
 	;; wait for first vblank
 	bit $2002
-	:
-		bit $2002
-		bpl :-
+:
+	bit $2002
+	bpl :-
 	;; clear all RAM to 0
 	lda #0
 	ldx #0
-	:
-		sta $0000, X
-		sta $0100, X
-		sta $0200, X
-		sta $0300, X
-		sta $0400, X
-		sta $0500, X
-		sta $0600, X
-		sta $0700, X
-		inx
-		bne :-
+:
+	sta $0000, X
+	sta $0100, X
+	sta $0200, X
+	sta $0300, X
+	sta $0400, X
+	sta $0500, X
+	sta $0600, X
+	sta $0700, X
+	inx
+	bne :-
 	;; wait for second vblank
-	:
-		bit $2002
-		bpl :-
+:
+	bit $2002
+	bpl :-
 	;; NES is initialized, ready to begin!
 
-
-
-load_palettes:
-        lda $2002 ; reset PPU status
+        ;; load palettes
+        lda $2002               ; reset PPU status
 	lda #$3F
 	sta $2006
         lda #0
-	sta $2006 ; set PPU address to $3F00
+	sta $2006               ; set PPU address to $3F00
 	ldx #0
-	:
-		lda palette, X
-		sta $2007
-		inx
-		cpx #32
-	        bcc :-
+:
+	lda palette, X
+	sta $2007
+	inx
+	cpx #32
+	bcc :-
 
         ;; load hardcoded sprites data
         ldx #0
-        :
-                lda sprites, X
-                sta $0200, X
-                inx
-                cpx #16
-                bcc :-
+:
+        lda sprites, X
+        sta $0200, X
+        inx
+        cpx #16
+        bcc :-
 
         lda #%10000000          ; enable NMI
         sta $2000
 
-        lda #%00010000
+        lda #%00010000          ; enable sprites
         sta $2001
 
         ;; palette swap
         lda #0
-        :
-                sta $0202
-                sta $0203
-                adc #1
-                sta $0206
-	        adc #1
-                sta $020a
-	        adc #1
-                sta $020e
-                adc #1
-                jmp :-
+:
+        sta $0202
+        sta $0203
+        adc #1
+        sta $0206
+	adc #1
+        sta $020a
+	adc #1
+        sta $020e
+        adc #1
+        jmp :-
 
 
 
 .segment "OAM"
-oam: .res 256        ; sprite OAM data to be uploaded by DMA
+oam: .res 256                   ; sprite OAM data to be uploaded by DMA
 
 .segment "CODE"
 nmi:
-	; sprite OAM DMA
+	;; sprite OAM DMA
 	ldx #0
 	stx $2003
 	lda #>oam
 	sta $4014
         rti
 
-;
-; irq
-;
+;;
+;; irq
+;;
 
 .segment "CODE"
 irq:
@@ -158,7 +156,3 @@ sprites:
 .byte $80,$04,$01,$88
 .byte $80,$04,$02,$90
 .byte $80,$04,$03,$98
-
-;
-; end of file
-;
